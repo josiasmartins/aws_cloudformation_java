3c354260d880049cc7b6ab84d379be65bc975352
Caitano <josias.m.caitano@accenture.com>
2024-04-24 15:38:15 -0300

    feature: configuracao do target group

---
+++ src/main/java/com/myorg/Service01Stack.java
@@ -0,0 +1,66 @@
+package com.myorg;
+
+import org.jetbrains.annotations.Nullable;
+import software.amazon.awscdk.RemovalPolicy;
+import software.amazon.awscdk.Stack;
+import software.amazon.awscdk.StackProps;
+import software.amazon.awscdk.services.appmesh.HeaderMatch;
+//import software.amazon.awscdk.services.ecs.*;
+import software.amazon.awscdk.services.ecs.AwsLogDriverProps;
+import software.amazon.awscdk.services.ecs.Cluster;
+import software.amazon.awscdk.services.ecs.ContainerImage;
+import software.amazon.awscdk.services.ecs.LogDriver;
+import software.amazon.awscdk.services.ecs.patterns.ApplicationLoadBalancedFargateService;
+import software.amazon.awscdk.services.ecs.patterns.ApplicationLoadBalancedTaskImageOptions;
+//import software.amazon.awscdk.services.elasticloadbalancingv2.HealthCheck;
+import software.amazon.awscdk.services.logs.LogGroup;
+import software.constructs.Construct;
+
+import software.amazon.awscdk.services.elasticloadbalancingv2.HealthCheck;
+
+
+//import java.util.Stack;
+
+public class Service01Stack extends Stack {
+
+    public Service01Stack(final Construct scope, final String id, Cluster cluster) {
+        this(scope, id, null, cluster);
+    }
+
+    public Service01Stack(final Construct scope, final String id, final StackProps props, Cluster cluster) {
+        super(scope, id, props);
+
+        ApplicationLoadBalancedFargateService service01 = ApplicationLoadBalancedFargateService.Builder.create(this, "ALB01")
+                .serviceName("service-01")
+                .cluster(cluster)
+                .cpu(512)
+                .desiredCount(2) // quantidade de instancias
+                .listenerPort(8008)
+                .memoryLimitMiB(1024)
+                .taskImageOptions(
+                        ApplicationLoadBalancedTaskImageOptions.builder()
+                                .containerName("aws_project01")
+                                .image(ContainerImage.fromRegistry("josiasmartins/spring_aws_ecs_fargate_cdk:0.0.1"))
+                                .containerPort(8080)
+                                .logDriver(LogDriver.awsLogs(AwsLogDriverProps.builder()
+                                        .logGroup(LogGroup.Builder.create(this, "Service01LogGroup")
+                                                .logGroupName("Service01")
+                                                .removalPolicy(RemovalPolicy.DESTROY)
+                                                .build())
+                                        .streamPrefix("Service01")
+                                        .build()
+                                )
+                        ).build()
+                )
+                .publicLoadBalancer(true) // diz que seja publico
+                .build();
+
+        service01.getTargetGroup().configureHealthCheck(HealthCheck.builder()
+                        .path("/actuator/health")
+                        .port("8080")
+                        .healthyGrpcCodes("200")
+                        .build());
+
+    }
+
+}
